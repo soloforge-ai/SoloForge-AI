96b4a2bbbe04d563988a89793a73cf8776b3a760
soloforge-ai <photonzz@hotmail.com>
2026-08-14 08:30:39 +0700

    Fix Asset Forge test package import

--- frontend/test/asset_forge_page_test.dart
+++ frontend/test/asset_forge_page_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/pages/asset_forge_page.dart';
+import 'package:frontend/pages/asset_forge_page.dart';
 
 void main() {
   testWidgets('Asset Forge page renders the MVP controls', (tester) async {
