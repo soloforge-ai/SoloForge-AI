854d82f5a85aeaa6017c3075cb3d9e899c22e63a
soloforge-ai <photonzz@hotmail.com>
2026-08-22 22:33:26 +0700

    test(asset-forge): add Flutter smoke test

---
+++ frontend/test/app_smoke_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/main.dart';
+
+void main() {
+  test('SoloForgeApp can be constructed', () {
+    const app = SoloForgeApp();
+    expect(app, isA<SoloForgeApp>());
+  });
+}
